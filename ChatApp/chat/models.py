47e83d1a90176b2fa138d5b661987cb833d7be0e
harshit17sharma09 <harshit17sharma09@gmail.com>
2018-06-07 12:06:31 +0530

    chat application

--- ChatApp/chat/models.py
+++ ChatApp/chat/models.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Message(models.Model):
+    sender = models.ForeignKey(User,on_delete=models.CASCADE,related_name ='sender')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver')
+    message = models.CharField(max_length =100)
+    timestamp = models.DateTimeField(auto_now_add = True)
+    is_read = models.BooleanField(default = False)
+    
+
+    def __str__(self):
+        return self.message
+    
+    class Meta:
+        ordering = ('timestamp',)
